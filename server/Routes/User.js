bd80c78cd03cfa6227dc078fa1c1e31bb8e839f3
anas ben raies <a.benraies19038@pi.tn>
2024-03-27 17:16:41 +0100

    add comments

--- server/Routes/User.js
+++ server/Routes/User.js
@@ -2,7 +2,7 @@ const UserRoute =require("express").Router()
 const {login,getMessages,addMessage} =require("../Controllers/UserController")
 
 
-
+//user Routes
 UserRoute.post("/login",login) 
 UserRoute.get("/getmessages",getMessages)
 UserRoute.post("/addmessage",addMessage)
